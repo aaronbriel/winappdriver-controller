3eeff1b8a3147ff15e336429a9066a948fffe44b
Briel, Aaron <aaronbriel@gmail.com>
2017-06-16 12:31:33 -0500

    cleanup

--- dist/controller.js
+++ dist/controller.js
@@ -1,84 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let childProcess = require('child_process'), //require('cross-spawn'),//
-//cmd = require('node-cmd'),
-//promise = require('bluebird').Promise,
-fs = require('fs'), 
-// path = require('path'),
-//http = require('http'),
-shell = require('shelljs'), retries = 0; //,
-//getAsync = Promise.promisify(cmd.get, { multiArgs: true, context: cmd });
+let childProcess = require('child_process'), fs = require('fs'), shell = require('shelljs'), retries = 0;
 exports.startWinAppDriver = (options) => {
     options = options || {};
-    let //wadPath = options.path !== undefined ? options.path : 'C:\\Program Files (x86)\\Windows Application Driver\\',
-    host = options.host !== undefined ? options.host : '127.0.0.1', port = options.port !== undefined ? options.port : '4723', shutdown = options.shutdown !== undefined ? options.shutdown : true, logDir = options.logDir !== undefined ? options.logDir : 'logs', command = 'start cmd.exe /K WinAppDriver.exe'; // + ' ' + host + ' ' + port; //
-    //command = 'WinAppDriver.exe';// + ' ' + host + ' ' + port; //wadPath +
-    // port += '/wd/hub';
+    let host = options.host !== undefined ? options.host : '127.0.0.1', port = options.port !== undefined ? options.port : '4723', shutdown = options.shutdown !== undefined ? options.shutdown : true, logDir = options.logDir !== undefined ? options.logDir : 'logs', command = 'start cmd.exe /K WinAppDriver.exe ' + host + ' ' + port;
     if (shutdown)
-        exports.stopWinAppDriver(); //{port:port}
+        exports.stopWinAppDriver();
     console.log('Starting WinAppDriver...');
     if (!fs.existsSync(logDir))
         fs.mkdirSync(logDir);
-    //shell.exec(command);
-    // let out = fs.openSync(path.join(logDir, 'winappdriver'), 'w');
-    // let er = fs.openSync(path.join(logDir, 'winappdriver-error'), 'w');
-    //still doesn't spawn.. maybe try https://www.npmjs.com/package/node-cmd with promises
-    //exec is CLOSEST SOLUTION HERE
-    //let child =
     childProcess.exec(command);
-    //let child = childProcess.spawn(//
-    //     'start',//command,
-    //     ['cmd.exe', '/K', 'WinAppDriver.exe'],
-    //     {
-    //         detached: true,
-    //         stdio: ['inherit', out, er]//stdio: ['ignore', out, er]//
-    //     }
-    // ).on('error', (err:any) => {
-    //     throw err
-    // });
-    exports.statusCheck(host, port); //child,
-    // setTimeout(function () {
-    //     startWinAppDriver({
-    //         path:options.path,
-    //         host:options.host,
-    //         port:options.port,
-    //         logDir:options.logDir,
-    //         shutdown:false});
-    // }, 1000)
+    exports.statusCheck(host, port);
 };
-// export const statusCheck = (host: string, port: string, child: any, statusCode: number,
-//     wdPath = '/wd/hub/status', m
-//
-// axRetries = 20) => {
-//
-//     retries += 1;
-//
-//     http.get({
-//         host: host,
-//         port: port,
-//         path: wdPath
-//     }, (res:any) => {
-//         statusCode = res.statusCode;
-//     }).on('error', (err:any) => {
-//         if (retries === maxRetries) {
-//             console.log('Connection was refused after ' + maxRetries + ' attempts.');
-//             console.log(err);
-//             throw err;
-//         }
-//         return err;
-//     });
-//
-//     if (statusCode === 200) {
-//         console.log('WinAppDriver is running on ' + host + ':' + port + '!');
-//         child.unref();
-//         retries = 0;
-//         statusCode = 0;
-//     } else {
-//         setTimeout(function () {
-//             statusCheck(host, port, child, statusCode);
-//         }, 1000)
-//     }
-// };
 exports.statusCheck = (host, port, maxRetries = 30) => {
     retries += 1;
     let command = 'netstat -a -o -n | ' +
@@ -87,7 +20,6 @@ exports.statusCheck = (host, port, maxRetries = 30) => {
     shell.exec(command, function (error, stdout) {
         if (stdout.length > 0) {
             console.log('WinAppDriver is running on ' + host + ':' + port + '!');
-            //child.unref();
             process.exit();
             retries = 0;
         }
@@ -103,24 +35,7 @@ exports.statusCheck = (host, port, maxRetries = 30) => {
     }
 };
 exports.stopWinAppDriver = () => {
-    //THIS WORKS BUT THROWS ERROR IN CONSOLE
-    // try {
-    //     shell.exec("taskkill /F /IM cmd.exe");
-    // } catch (err) {
-    //     console.log('WinAppDriver is shutdown, err caught.');
-    // }
-    // shell.exec("taskkill /F /IM WinAppDriver.exe"); //WORKS BUT LEAVES CMD WINDOW OPEN
     shell.exec('taskkill /F /IM cmd.exe /FI "windowtitle eq C:\\WINDOWS\\system32\\cmd.exe - WinAppDriver.exe" /T');
-    // options = options || {};
-    //
-    // let msg = 'WinAppDriver is shutdown',
-    //     port = options.port !== undefined ? options.port : '4723';
-    //
-    // shell.exec('for /f "tokens=5" %p in (\'netstat -a -o -n ^| ' +
-    //     'findstr "LISTENING" ^| ' +
-    //     'findstr ":' + port + '"\') do ( taskkill -F -PID %p )');
-    // //shell.exec('call nonsecureSendKeys.bat "WinAppDriver.exe" "{ENTER}"');
-    // //https://stackoverflow.com/questions/30419836/how-do-i-paste-text-to-a-console-window-of-an-specific-process-id-via-command-li
     console.log('WinAppDriver is shutdown');
 };
 //# sourceMappingURL=controller.js.map
--- src/controller.ts
+++ src/controller.ts
@@ -1,100 +1,30 @@
-let childProcess = require('child_process'),//require('cross-spawn'),//
-    //cmd = require('node-cmd'),
-    //promise = require('bluebird').Promise,
+let childProcess = require('child_process'),
     fs = require('fs'),
-    // path = require('path'),
-    //http = require('http'),
     shell = require('shelljs'),
-    retries = 0;//,
-    //getAsync = Promise.promisify(cmd.get, { multiArgs: true, context: cmd });
-
+    retries = 0;
 
 export const startWinAppDriver = (options?: any) => {
 
     options = options || {};
 
-    let //wadPath = options.path !== undefined ? options.path : 'C:\\Program Files (x86)\\Windows Application Driver\\',
-        host = options.host !== undefined ? options.host : '127.0.0.1',
+    let host = options.host !== undefined ? options.host : '127.0.0.1',
         port = options.port !== undefined ? options.port : '4723',
         shutdown = options.shutdown !== undefined ? options.shutdown : true,
         logDir = options.logDir !== undefined ? options.logDir : 'logs',
-        command = 'start cmd.exe /K WinAppDriver.exe';// + ' ' + host + ' ' + port; //
-        //command = 'WinAppDriver.exe';// + ' ' + host + ' ' + port; //wadPath +
-
-    // port += '/wd/hub';
+        command = 'start cmd.exe /K WinAppDriver.exe ' + host + ' ' + port;
 
     if (shutdown)
-        stopWinAppDriver(); //{port:port}
+        stopWinAppDriver();
 
     console.log('Starting WinAppDriver...');
 
     if (!fs.existsSync(logDir))
         fs.mkdirSync(logDir);
 
-    //shell.exec(command);
-
-    // let out = fs.openSync(path.join(logDir, 'winappdriver'), 'w');
-    // let er = fs.openSync(path.join(logDir, 'winappdriver-error'), 'w');
-
-    //still doesn't spawn.. maybe try https://www.npmjs.com/package/node-cmd with promises
-    //exec is CLOSEST SOLUTION HERE
-    //let child =
     childProcess.exec(command);
-    //let child = childProcess.spawn(//
-    //     'start',//command,
-    //     ['cmd.exe', '/K', 'WinAppDriver.exe'],
-    //     {
-    //         detached: true,
-    //         stdio: ['inherit', out, er]//stdio: ['ignore', out, er]//
-    //     }
-    // ).on('error', (err:any) => {
-    //     throw err
-    // });
 
-    statusCheck(host, port); //child,
-    // setTimeout(function () {
-    //     startWinAppDriver({
-    //         path:options.path,
-    //         host:options.host,
-    //         port:options.port,
-    //         logDir:options.logDir,
-    //         shutdown:false});
-    // }, 1000)
+    statusCheck(host, port);
 };
-
-// export const statusCheck = (host: string, port: string, child: any, statusCode: number,
-//     wdPath = '/wd/hub/status', m
-//
-// axRetries = 20) => {
-//
-//     retries += 1;
-//
-//     http.get({
-//         host: host,
-//         port: port,
-//         path: wdPath
-//     }, (res:any) => {
-//         statusCode = res.statusCode;
-//     }).on('error', (err:any) => {
-//         if (retries === maxRetries) {
-//             console.log('Connection was refused after ' + maxRetries + ' attempts.');
-//             console.log(err);
-//             throw err;
-//         }
-//         return err;
-//     });
-//
-//     if (statusCode === 200) {
-//         console.log('WinAppDriver is running on ' + host + ':' + port + '!');
-//         child.unref();
-//         retries = 0;
-//         statusCode = 0;
-//     } else {
-//         setTimeout(function () {
-//             statusCheck(host, port, child, statusCode);
-//         }, 1000)
-//     }
-// };
 
 export const statusCheck = (host: string, port: string, maxRetries=30) => { //child: any,
 
@@ -108,7 +38,6 @@ export const statusCheck = (host: string, port: string, maxRetries=30) => { //ch
         function(error: any, stdout: any){
             if (stdout.length > 0) {
                 console.log('WinAppDriver is running on ' + host + ':' + port + '!');
-                //child.unref();
                 process.exit();
                 retries = 0;
             } else {
@@ -124,26 +53,7 @@ export const statusCheck = (host: string, port: string, maxRetries=30) => { //ch
     }
 };
 
-export const stopWinAppDriver = () => { //options?:any
-
-    //THIS WORKS BUT THROWS ERROR IN CONSOLE
-    // try {
-    //     shell.exec("taskkill /F /IM cmd.exe");
-    // } catch (err) {
-    //     console.log('WinAppDriver is shutdown, err caught.');
-    // }
-    // shell.exec("taskkill /F /IM WinAppDriver.exe"); //WORKS BUT LEAVES CMD WINDOW OPEN
+export const stopWinAppDriver = () => {
     shell.exec('taskkill /F /IM cmd.exe /FI "windowtitle eq C:\\WINDOWS\\system32\\cmd.exe - WinAppDriver.exe" /T');
-
-    // options = options || {};
-    //
-    // let msg = 'WinAppDriver is shutdown',
-    //     port = options.port !== undefined ? options.port : '4723';
-    //
-    // shell.exec('for /f "tokens=5" %p in (\'netstat -a -o -n ^| ' +
-    //     'findstr "LISTENING" ^| ' +
-    //     'findstr ":' + port + '"\') do ( taskkill -F -PID %p )');
-    // //shell.exec('call nonsecureSendKeys.bat "WinAppDriver.exe" "{ENTER}"');
-    // //https://stackoverflow.com/questions/30419836/how-do-i-paste-text-to-a-console-window-of-an-specific-process-id-via-command-li
     console.log('WinAppDriver is shutdown');
 };
